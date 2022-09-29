25f1ffbb8bd22587b56cb5209316b6f0a18a2dc0
이석호 <mercen@mercen.net>
2022-09-29 12:00:27 +0900

    Initial Commit

---
+++ EAT-IT-DOG/EAT_IT_DOGApp.swift
@@ -0,0 +1,17 @@
+//
+//  EAT_IT_DOGApp.swift
+//  EAT-IT-DOG
+//
+//  Created by Mercen on 2022/09/29.
+//
+
+import SwiftUI
+
+@main
+struct EAT_IT_DOGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
