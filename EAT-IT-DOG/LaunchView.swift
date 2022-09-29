fd41555a4c249ef4a5578eba4c0df0420521f500
이석호 <mercen@mercen.net>
2022-09-30 07:07:31 +0900

    ADD: Launch Animation

--- EAT-IT-DOG/LaunchView.swift
+++ EAT-IT-DOG/LaunchView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct LaunchView: View {
     
     // State Variable
-    @State private var animated: Bool = false
+    @State private var animated: [Bool] = [false, false, false]
     @State private var launched: Bool = false
     
     var body: some View {
@@ -23,25 +23,42 @@ struct LaunchView: View {
                 Color.accentColor.ignoresSafeArea()
                 VStack {
                     Spacer()
-                    Text("먹어보시개")
-                        .font(.system(size: 43))
-                        .padding(.bottom)
-                    Text("세상의 모든 좋은 음식이\n반려견에게 닿을 때까지")
-                        .font(.title)
-                        .fontWeight(.medium)
-                        .multilineTextAlignment(.center)
+                    if animated[0] {
+                        Text("먹어보시개")
+                            .font(.system(size: 43))
+                            .fontWeight(.medium)
+                            .transition(.opacity)
+                            .frame(height: 70)
+                    }
+                    if animated[1] {
+                        Text("세상의 모든 좋은 음식이\n반려견에게 닿을 때까지")
+                            .font(.title)
+                            .multilineTextAlignment(.center)
+                            .transition(.opacity)
+                            .frame(height: 70)
+                    }
                     Spacer()
-                    Image("Dog")
-                        .resizable()
-                        .frame(width: 235, height: 269)
-                        .padding(.leading, 100)
+                    if animated[2] {
+                        Image("Dog")
+                            .resizable()
+                            .frame(width: 235, height: 269)
+                            .padding(.leading, 100)
+                            .transition(.opacity)
+                            .frame(height: 300)
+                    }
                 }
                 .foregroundColor(.white)
                 .padding(40)
             }
             .transition(.backslide)
             .onAppear {
-                animated.toggle()
+                for idx in 0..<3 {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + CGFloat(idx)/4, execute: {
+                        withAnimation(.easeInOut(duration: 1)) {
+                            animated[idx].toggle()
+                        }
+                    })
+                }
                 DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
                     withAnimation(.easeInOut) {
                         launched.toggle()
